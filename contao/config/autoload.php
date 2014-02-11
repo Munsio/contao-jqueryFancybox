b09368609197f7be7cdc775905c90e36569e4c5c
Mr. Tool <github@r2pi.de>
2014-02-11 16:04:43 +0100

    change direcotry structure for composer

---
+++ contao/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package JqueryFancybox
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_fancybox' => 'system/modules/jqueryFancybox/templates/jquery',
+));
---
+++ contao/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+define('FANCYBOX', '2.1.5');
